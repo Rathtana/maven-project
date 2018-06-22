f7b2a59ba1e3bc9f80556d3d820228a37e4bbe81
Rathtana Duong <drathtana@yahoo.com>
2018-06-21 23:30:47 -0700

    remove white space

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -4,14 +4,14 @@ package com.example;
  * This is a class.
  */
 public class Greeter {
-	
+
 	/**
 	 * This is a constructor.
 	 */
 	public Greeter() {
-		
-	}
 	
+	}
+
 	/**
 	 * @param someone the name of a person
 	 * @return greeting string
