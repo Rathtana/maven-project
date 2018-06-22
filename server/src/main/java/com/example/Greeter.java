d36df48439d7eeb2ab3b04d852a40adc702f6f42
Rathtana Duong <drathtana@yahoo.com>
2018-06-21 23:14:21 -0700

    checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,4 +1,3 @@
-
 package com.example;
 
 /**
@@ -14,7 +13,7 @@ public class Greeter {
 	}
 	
 	//TODO: Add javadoc comment
-	public String greet(final String someone) {
+	public final String greet(final String someone) {
 		return String.format("Hello, %s!", someone);
 	}
 }
